8a3e0642cbcdc43b5b07d5cc7902c8b02612ee45
Juan Tejeda <juantejeda@Juans-MBP.attlocal.net>
2023-10-26 16:22:25 -0500

    added templates

--- project/routes.py
+++ project/routes.py
@@ -1,5 +1,8 @@
 from project import app
+from flask import render_template
 
 @app.route('/')
 def index():
-    return "Destination: Index"
+    my_name='Juan'
+    count=[1, 2, 3, 4, 5, 6]
+    return render_template('index.html', name=my_name, count=count)
