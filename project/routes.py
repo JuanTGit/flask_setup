5a0380413250c258fcc180b94dd4d0c536978f28
Juan Tejeda <j.tejeda46@yahoo.com>
2023-10-26 23:36:00 -0500

    added registry page and updated forms

--- project/routes.py
+++ project/routes.py
@@ -1,11 +1,17 @@
 from project import app
 from flask import render_template
+from project.forms import RegisterForm
 
 @app.route('/')
 def index():
     my_name='Juan'
     count=[1, 2, 3, 4, 5, 6]
     return render_template('index.html', name=my_name, count=count)
+
+@app.route('/register')
+def register():
+    form = RegisterForm()
+    return render_template('register.html', form=form)
 
 @app.route('/login')
 def login():
