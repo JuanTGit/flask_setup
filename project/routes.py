731877ef693f7a8291bdf90895cd987f82ebdddf
Juan Tejeda <j.tejeda46@yahoo.com>
2023-10-27 13:40:55 -0500

    Added Post request to registration.

--- project/routes.py
+++ project/routes.py
@@ -8,9 +8,14 @@ def index():
     count=[1, 2, 3, 4, 5, 6]
     return render_template('index.html', name=my_name, count=count)
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        email = form.email.data
+        password = form.password.data
+        print(username, email, password)
     return render_template('register.html', form=form)
 
 @app.route('/login')
