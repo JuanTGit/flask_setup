1b24f22b3b74d5b4b8783b3696647d9287912afd
Juan Tejeda <j.tejeda46@yahoo.com>
2023-10-30 16:09:46 -0500

    working login

--- project/routes.py
+++ project/routes.py
@@ -3,8 +3,9 @@ from project import app
 # Used for loading in our templates from html files
 from flask import render_template, redirect, url_for
 # Forms for users to input data
-from project.forms import RegisterForm
+from project.forms import RegisterForm, LoginForm
 from project.models import User
+from flask_login import login_user
 
 @app.route('/')
 def index():
@@ -40,6 +41,20 @@ def register():
         return redirect(url_for('index'))
     return render_template('register.html', form=form)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+
+        user = User.query.filter_by(username=username).first()
+
+        if not user or not user.check_password(password):
+            print('Username or Password is incorrect.')
+            return redirect(url_for('login'))
+        login_user(user)
+        return redirect(url_for('index'))
+        
+
+    return render_template('login.html', form=form)
