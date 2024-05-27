f81147e2dcb4382930503c4dca258e9e6eb3becd
Juan <j.tejeda46@yahoo.com>
2024-05-26 22:27:54 -0500

    Created POST request for adding a new user to db through json dict

--- project/blueprints/api/routes.py
+++ project/blueprints/api/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from project.blueprints.auth.models import User
 from project.blueprints.products.models import Product
 
@@ -18,3 +18,35 @@ def get_user(id):
 def get_products():
     products = Product.query.all()
     return jsonify([p.to_dict() for p in products])
+
+# Create, Retreive, Update, Delete
+
+# Create a User
+@api.route('/users', methods=['POST'])
+def create_user():
+    data = request.json
+    # Validate Data
+    for field in ["username", "email", "password", "confirm_password"]:
+        if field not in data:
+            return jsonify({'error': f"You are missing {field} field"}), 400
+    if data["password"] != data["confirm_password"]:
+        return jsonify({'error': f"Passwords do not match"}), 400
+    
+    # Grab data from the request body
+    username = data["username"]
+    email = data["email"]
+    password = data["password"]
+    confirm_password = data["confirm_password"]
+
+    # Check if username or email already exist in db
+    user_exists = User.query.filter((User.username == username)|(User.email == email)).all()
+    if user_exists:
+        return jsonify({'error': f'User with username {username} or email {email} already exists.'}), 400
+    
+    # Create new user
+    new_user = User(username=username, email=email, password=password)
+
+    return jsonify(new_user.to_dict())
+
+# Update User
+
